268799abbc3a8b6961b1a2d4ae86b2fd46f691c7
Oussema sferi <62106784+oussema-sferi@users.noreply.github.com>
2020-11-17 00:21:22 +0100

    fetch products based on selected category done!

--- src/Controller/StoreOrdersController.php
+++ src/Controller/StoreOrdersController.php
@@ -4,8 +4,13 @@ namespace App\Controller;
 
 use App\Entity\Category;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
+
 
 class StoreOrdersController extends AbstractController
 {
@@ -31,5 +36,24 @@ class StoreOrdersController extends AbstractController
         ]);
     }
 
+    /**
+     * @Route("/manager/store/fetch", name="fetch_products")
+     */
+    public function fetchProducts(Request $request): Response
+    {
+        if($request->isXmlHttpRequest()) {
+            $id = $request->get('id');
+            $category = $this->getDoctrine()->getRepository(Category::class)->find($id);
+            if($category instanceof Category) {
+                $products = $category->getProducts();
+                $serializer = new Serializer([new ObjectNormalizer()]);
+                $result = $serializer->normalize($products,'json',['attributes' => ['id','name','price']]);
+                return new JsonResponse($result);
+            } else {
+                return new JsonResponse(['message'=> 'Category not found!']);
+            }
+        }
+        return new Response('use Ajax');
+    }
 
 }
